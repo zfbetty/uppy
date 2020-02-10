0cbb20395b8f7eec82b4dc3b1ab22f61f902ca19
Renée Kooi <renee@kooi.me>
2020-02-10 15:31:25 +0100

    Use Babel's strict compilation mode
    This fixes some correctness issues, primarily that you could call the
    core `Uppy` _class_ as a function. The loose compiler mode doesn't check
    that you're actually doing `new Uppy()`. Our docs show `Uppy()`
    everywhere without the `new`, because our default/main export is a
    factory function, but you can import the Uppy _class_ by doing
    ```js
    const { Uppy } = require('@uppy/core')
    ```
    and if you call _that_, it results in a very unhelpful error message, as
    seen here: https://community.transloadit.com/t/cant-get-uppy-to-work-with-react/15202/1
    
    Babel adds a lot of helper functions in the strict mode which added
    several kB to each package. So I added `@babel/runtime` to deduplicate a
    lot of them. With this, each package gains about 1kB, which is still
    significant but the effect is less (at most 4kB) if you use many packages
    because they can share the helper functions.
    It's also quite possible that `@babel/runtime` is in use by other
    dependencies in a larger application, so the effect would be even less.

--- babel.config.js
+++ babel.config.js
@@ -7,12 +7,17 @@ module.exports = (api) => {
   return {
     presets: [
       ['@babel/preset-env', {
-        modules: false,
-        loose: true,
+        modules: 'commonjs',
+        loose: false,
         targets
       }]
     ],
     plugins: [
+      ['@babel/plugin-transform-runtime', {
+        corejs: false,
+        useESModules: false,
+        version: '^7.8.2'
+      }],
       ['@babel/plugin-proposal-class-properties', { loose: true }],
       '@babel/plugin-transform-object-assign',
       ['@babel/plugin-transform-react-jsx', { pragma: 'h' }],
